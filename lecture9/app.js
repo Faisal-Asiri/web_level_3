9c79a8162a8b9e84b7a70fcd852ef03653f5ebd4
unknown <faisal.761@hotmail.com>
2023-11-18 01:36:08 +0300

    lecture9

---
+++ lecture9/app.js
@@ -0,0 +1,18 @@
+(function (){
+'use strict';
+angular.module('DIApp', [])
+.controller('DIController',DIController);
+function DIController ($scope, $filter,$injector){
+    $scope.name="Faisal";
+    $scope.upper =function(){
+        var upCase= $filter('uppercase');
+        $scope.name =upCase($scope.name);
+
+    };
+    console.log($injector.annotate(DIController));
+}
+function AnnoateMate(name, job, blah){
+    return "Blah!";
+}
+console.log(DIController.toString());
+})();
